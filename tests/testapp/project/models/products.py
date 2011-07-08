fb7c973ef2e53eb36b8f65650882113573c8edab
zeus <gelios@gmail.com>
2011-07-08 15:58:13 +0400

    New tests

--- tests/testapp/project/models/products.py
+++ tests/testapp/project/models/products.py
@@ -5,19 +5,31 @@ from shop.util.fields import CurrencyField
 
 
 class BookProduct(Product):
+    class Meta(object):
+        app_label = 'project'
+
     isbn = models.CharField(max_length=255)
     number_of_pages = models.IntegerField()
 
 
 class CompactDiscProduct(Product):
+    class Meta(object):
+        app_label = 'project'
+
     number_of_tracks = models.IntegerField()
 
 
 class BaseProduct(models.Model):
+    class Meta(object):
+        app_label = 'project'
+
     unit_price = CurrencyField()
 
 
 class ProductVariation(Product):
+    class Meta(object):
+        app_label = 'project'
+        
     baseproduct = models.ForeignKey(BaseProduct)
 
     def get_price(self):
